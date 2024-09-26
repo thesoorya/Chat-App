7e891dd47867957abb4193eecee6d6720abcec4c
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-09-26 17:04:39 +0530

    chat app server completed

--- server/server.js
+++ server/server.js
@@ -2,25 +2,40 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const http = require('http')
-const {Server} = require('socket.io')
+const { Server } = require('socket.io')
 
 app.use(cors())
 
 const server = http.createServer(app)
 
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
+    console.log('User connected:', socket.id);
+
+    socket.on('join_room', (data) => {
+        console.log('User joining room:', data);
+        socket.join(data);
+        console.log(`User ${socket.id} joined room ${data}`);
+    });
+
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('receive_message', data)
+    })
 
     socket.on('disconnect', () => {
-        console.log('User Disconnected', socket.id);
-        
+        console.log('User Disconnected:', socket.id);
+
     })
-    
+
 })
 
 server.listen(5000, () => {
     console.log('Server Started');
-    
+
 })
