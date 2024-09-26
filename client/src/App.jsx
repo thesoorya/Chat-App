2a32e563325f930630e1e71874b776c148e311e5
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-09-26 17:05:10 +0530

    join room func. completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
+import ChatBox from "./ChatBox";
+
+const socket = io.connect("http://localhost:5000");
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    username: "",
+    roomId: "",
+  });
+  const [showChat, setShowChat] = useState(false);
 
-export default App
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const joinRoom = (e) => {
+    e.preventDefault();
+    if (formData.username !== "" && formData.roomId !== "") {
+      socket.emit("join_room", formData.roomId);
+    }
+    setShowChat(true)
+  };
+
+  return (
+    <div className="app">
+      {!showChat ? (
+        <div className="card">
+          <span className="card__title">Join Room</span>
+          <form className="card__form" onSubmit={joinRoom}>
+            <input
+              placeholder="Username"
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+            <input
+              placeholder="Room ID"
+              type="text"
+              name="roomId"
+              value={formData.roomId}
+              onChange={handleChange}
+              required
+            />
+            <button type="submit" className="sign-up">
+              Join Now
+            </button>
+          </form>
+        </div>
+      ) : (
+        <ChatBox socket={socket} data={formData} />
+      )}
+    </div>
+  );
+};
+
+export default App;
